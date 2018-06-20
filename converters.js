a08e30c1eb8647131324452eaa1481bafbf5f60b
Jeff Hykin <jeff.hykin@gmail.com>
2018-06-20 03:03:03 -0500

    _

---
+++ converters.js
@@ -0,0 +1,65 @@
+// A class for indicating a value is invalid
+// it is used for input validation
+export class Invalid {
+    constructor(value, errorMsg) {
+        this.value = value
+        this.errorMsg = errorMsg
+    }
+    valueOf(){
+        return this.value
+    }
+    toString(){
+        return this.value
+    }
+}
+export function isInvalid(value) {
+    if (typeof value == "object" && value instanceof Invalid) {
+        return true
+    }
+    return false
+}
+
+export default {
+    bool: {
+        stateToUserConverter: (shouldBeBool) => {
+            if (shouldBeBool === true) {
+                return "True"
+            } else if (shouldBeBool === false) {
+                return "False"
+            }
+            // If provided value is not boolean, then return the value itself
+            return shouldBeBool
+        },
+        userInputToStateConverter: (userInput) => {
+            var userInputLowerCased = userInput.toLowerCase()
+            if (userInputLowerCased == "true") {
+                return true
+            } else if (userInputLowerCased == "false") {
+                return false
+            }
+            // if not true or false, then its invalid
+            return new Invalid(userInput)
+        }
+    },
+    digits: {
+        userInputToStateConverter: (userInput)=>{
+            if (userInput.match(/\d+/)) {
+                // convert string to number
+                return userInput-0
+            }
+            // if its not a number
+            return new Invalid(userInput, "Please only input numerical digits (0-9)")
+        }
+    },
+    "datetime-local": {
+        stateToUserConverter : (shouldBeDateTime) => {
+            if (shouldBeDateTime && shouldBeDateTime instanceof Date) {
+                return shouldBeDateTime.toISOString().substring(0, 16)
+            }
+            return shouldBeDateTime
+        },
+        userInputToStateConverter: (userInput) => {
+            return new Date(userInput+'Z')
+        }
+    }
+}
---
+++ index.js
@@ -0,0 +1,64 @@
+import converters, {Invalid, isInvalid} from 'converters'
+
+export var isInvalid = isInvalid
+export var Invalid = Invalid
+
+HandleChange = (thisFromComponent, stateAttribute, userInputToStateConverter=null) => event => {
+    // create a copy of state instead of mutating the original
+    var newValue = event.target.value
+    // if there is a converter function, then run the function before it returns to state
+    // for example convert "True" into the boolean: true, or convert the string "Jan 12 2017" to dateTime(1,12,2017)
+    if (userInputToStateConverter) {
+        newValue = userInputToStateConverter(newValue)
+    }
+    eval("copyOfState."+stateAttribute+" = newValue")
+    // if the Attribute is not nested 
+    // update the compoenent state once with the new state
+    thisFromComponent.setState(copyOfState)
+};
+
+
+export default Input = function({linkTo, className="input-box", classAdd="", ...otherProps}) 
+    {
+        // add additional classes
+        className = className + " " + classAdd
+        // add error class if there is an "invalid" prop
+        className = otherProps.invalid?"input-err "+className : className
+        
+        // 
+        // Controlled input
+        // 
+        if (otherProps.this && linkTo) {
+            // get the value from the component's state
+            var valueFromState = retrieveKeyValueNoExceptions(otherProps.this.state,"."+linkTo)
+            if (isInvalid(valueFromState)) {
+                // add error class if the value is invalid
+                className = "input-error "+className
+            }
+            // retrieve converters
+            var stateToUserConverter      = retrieveKeyValueNoExceptions(converters,"['"+otherProps.type+"'].stateToUserConverter")
+            var userInputToStateConverter = retrieveKeyValueNoExceptions(converters,"['"+otherProps.type+"'].userInputToStateConverter")
+            // convert the display value if needed
+            if (stateToUserConverter) {
+                valueFromState = stateToUserConverter(valueFromState)
+            }
+            // always convert null values to "" (otherwise react will complain)
+            if (valueFromState == null) {
+                valueFromState = ""
+            }
+            return <input value={valueFromState} onChange={HandleChange(otherProps.this, linkTo, userInputToStateConverter)} className={className}  {...otherProps} />
+        }
+        
+        // 
+        // uncontrolled input
+        // 
+        
+        // if there is a value, and if it is invalid
+        if (isInvalid(otherProps.value)) {
+            // add error class if the value is invalid
+            className = "input-error "+className
+            
+        }
+        
+        return <input className={className} {...otherProps} />
+    }
